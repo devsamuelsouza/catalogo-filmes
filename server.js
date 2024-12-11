ee8d823f3db60f0fca8400b7822b02de7894fa77
Samuca Silva <silva.samueldev@gmail.com>
2024-12-10 21:52:34 -0300

    :construction: desenvolvimento 10/12

--- server.js
+++ server.js
@@ -8,21 +8,30 @@ server.set('views', './views')
 server.use(express.static('public'));
 
 // Rotas
+
+//Home
 server.get('/', async (request, response) => {
+
     const filmes = []
 
-    for (var i = 277; i <= 278; i++) {
-        const url = `https://api.themoviedb.org/3/movie/${i}?api_key=${apikey}`
-        const reply = await fetch(url)
-        const data = await reply.json()
-        if (reply.status == 200) {
-            filmes.push(data)
-        }
-    }
-    response.render('index', {filmes})
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apikey}&language=pt-BR`
+    const url2 = `https://api.themoviedb.org/3/discover/movie?api_key=${apikey}&language=pt-BR&page=2`
+
+    const reply = await fetch(url)
+    const reply2 = await fetch(url2)
+
+    const data = await reply.json()
+    const data2 = await reply2.json()
+
+    filmes.push(data)
+    filmes.push(data2)
+    console.log(filmes)
+
+    response.render('index', { filmes })
 })
 
-server.get('/popular', ()=>{
+// Popular
+server.get('/popular', () => {
 
 })
 
